964b98a64a75332bf5cdbaeb7291605cce8500a6
Marcos Benatti Lauer <benatti77@gmail.com>
2018-01-05 06:46:16 -0200

    mix files

--- mix.exs
+++ mix.exs
@@ -7,25 +7,42 @@ defmodule DriveWealth.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      # Docs
+      name: "DriveWealth API",
+      source_url: "https://github.com/mbenatti/drivewealth_api",
+      homepage_url: "https://github.com/mbenatti/drivewealth_api/README.md"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
      {:poison, "~> 3.0"},
      {:tesla, "~> 0.10.0"},
      {:hackney, "~> 1.10"},
-     {:credo, "~> 0.8", only: [:dev]}
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+     {:credo, "~> 0.8", only: :dev},
+     {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
+  end
+
+  defp package do
+    [maintainers: ["Marcos Benatti Lauer"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/mbenatti/drivewealth_api"},
+     files: ~w(mix.exs README.md lib)]
+  end
+
+  defp description do
+    """
+    DriveWealth API in Elixir (http://developer.drivewealth.com)
+    """
   end
 end
